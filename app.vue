5beb657f0375391e76e3f7328dc4b463afd15935
Muhammad-Ikhwan-Fathulloh <muhammadikhwanfathulloh17@gmail.com>
2024-12-12 10:44:55 +0700

    Update Metadata

--- app.vue
+++ app.vue
@@ -1,21 +1,5 @@
 <template>
   <div>
-    <head>
-      <title>Dinna & Fendy's Wedding Invitation by Nocturnailed</title>
-      <meta property="og:title" content="Dinna & Fendy's Wedding Invitation" />
-      <meta
-        property="og:description"
-        content="Join us to celebrate the wedding of Dinna and Fendy on December 22th, 2024. Tap to view more details."
-      />
-      <meta
-        property="og:image"
-        :content="`${baseUrl}/og-image.jpeg`"
-      />
-      <meta
-        property="og:url"
-        :content="`${baseUrl}/`"
-      />
-    </head>
     <NuxtPage />
   </div>
 </template>
@@ -27,5 +11,27 @@ const baseUrl = ref("http://localhost:3000");
 
 onMounted(() => {
   baseUrl.value = window.location.origin; // Ambil URL global saat mounted
+});
+
+useHead({
+  title: "Dinna & Fendy's Wedding Invitation",
+  meta: [
+    {
+      property: "og:title",
+      content: "Dinna & Fendy's Wedding Invitation",
+    },
+    {
+      property: "og:description",
+      content: "Join us to celebrate the wedding of Dinna and Fendy on December 22th, 2024. Tap to view more details.",
+    },
+    {
+      property: "og:image",
+      content: `${baseUrl.value}/og-image.jpeg`, // URL dinamis
+    },
+    {
+      property: "og:url",
+      content: `${baseUrl.value}/`, // URL dinamis
+    },
+  ],
 });
 </script>
