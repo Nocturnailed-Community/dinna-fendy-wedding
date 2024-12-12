2f19a7762e82da932a635103e406ddb24aca1dfd
Muhammad-Ikhwan-Fathulloh <muhammadikhwanfathulloh17@gmail.com>
2024-12-12 12:46:19 +0700

    Update Metadata

--- app.vue
+++ app.vue
@@ -4,82 +4,39 @@
   </div>
 </template>
 
-<script>
-export default {
-  data() {
-    return {
-      eventTitle: "Dinna & Fendy's Wedding Invitation by Nocturnailed",
-      eventDescription: "Join us to celebrate the wedding of Dinna and Fendy on December 22th, 2024. Tap to view more details.",
-      eventImage: "https://dinna-fendy.nocturnailed.com/og-image.jpeg",
-      eventURL: "https://dinna-fendy.nocturnailed.com/",
-    };
-  },
-  computed: {
-    metaTags() {
-      return [
-        {
-          hid: "og:title",
-          property: "og:title",
-          content: this.eventTitle,
-        },
-        {
-          hid: "og:description",
-          property: "og:description",
-          content: this.eventDescription,
-        },
-        {
-          hid: "og:image",
-          property: "og:image",
-          content: this.eventImage,
-        },
-        {
-          hid: "og:image:width",
-          property: "og:image:width",
-          content: "1200",
-        },
-        {
-          hid: "og:image:height",
-          property: "og:image:height",
-          content: "630",
-        },
-        {
-          hid: "og:url",
-          property: "og:url",
-          content: this.eventURL,
-        },
-        {
-          hid: "og:image:type",
-          property: "og:image:type",
-          content: "image/jpeg",
-        },
-        {
-          hid: "twitter:card",
-          name: "twitter:card",
-          content: "summary_large_image",
-        },
-        {
-          hid: "twitter:title",
-          name: "twitter:title",
-          content: this.eventTitle,
-        },
-        {
-          hid: "twitter:description",
-          name: "twitter:description",
-          content: this.eventDescription,
-        },
-        {
-          hid: "twitter:image",
-          name: "twitter:image",
-          content: this.eventImage,
-        },
-      ];
-    },
-  },
-  head() {
-    return {
-      title: this.eventTitle,
-      meta: this.metaTags,
-    };
-  },
-};
+<script setup>
+import { useRuntimeConfig } from "#app";
+
+// Mengambil runtime config
+const config = useRuntimeConfig();
+
+// Data event
+const eventTitle = "Dinna & Fendy's Wedding Invitation by Nocturnailed";
+const eventDescription =
+  "Join us to celebrate the wedding of Dinna and Fendy on December 22th, 2024. Tap to view more details.";
+
+// Menggunakan baseURL dari konfigurasi atau default ke localhost
+const baseURL = config.public?.baseURL || "https://dinna-fendy.nocturnailed.com";
+
+// Data dinamis
+const eventImage = `${baseURL}/og-image.jpeg`;
+const eventURL = baseURL;
+
+// Meta tags dinamis
+useHead({
+  title: eventTitle,
+  meta: [
+    { hid: "og:title", property: "og:title", content: eventTitle },
+    { hid: "og:description", property: "og:description", content: eventDescription },
+    { hid: "og:image", property: "og:image", content: eventImage },
+    { hid: "og:image:width", property: "og:image:width", content: "1200" },
+    { hid: "og:image:height", property: "og:image:height", content: "630" },
+    { hid: "og:url", property: "og:url", content: eventURL },
+    { hid: "og:image:type", property: "og:image:type", content: "image/jpeg" },
+    { hid: "twitter:card", name: "twitter:card", content: "summary_large_image" },
+    { hid: "twitter:title", name: "twitter:title", content: eventTitle },
+    { hid: "twitter:description", name: "twitter:description", content: eventDescription },
+    { hid: "twitter:image", name: "twitter:image", content: eventImage },
+  ],
+});
 </script>
